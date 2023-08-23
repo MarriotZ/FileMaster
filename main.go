3abfc8e99eee0a56735d30cc54c057b37f3a9368
marriotz <964439310@qq.com>
2023-08-23 22:07:08 +0800

    File Master by GO

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import _case "file-rw/case"
+
+func main() {
+	//_case.CopyDirToDir()
+	//_case.ReadWriteFiles()
+	//_case.OneSideReadWriteToDest()
+	//_case.ReadLine1()
+	//_case.ReadLine2()
+	_case.ReadLine3()
+}
